e9728660155c7b135dce08f796e2c9305a3bba39
John Evans <john.evans@digital.justice.gov.uk>
2023-01-26 09:12:25 +0000

    ✨ Update decode_secret to accept a namespace parameter  Also commit a load of stuff that I neglected to commit when I knew what the changes were about 😑

--- local/dump_them_cases.py
+++ local/dump_them_cases.py
@@ -5,21 +5,28 @@ import csv
 import json
 import requests
 import time
+from requests.auth import HTTPBasicAuth
 
-COURT_CASE_TOKEN = os.environ.get('COURT_CASE_TOKEN')
-CHER_TOKEN = os.environ.get('CHER_TOKEN')
-CCS_HOST = "https://court-case-service-preprod.apps.live-1.cloud-platform.service.justice.gov.uk"
-CHER_HOST = "https://court-hearing-event-receiver-preprod.hmpps.service.justice.gov.uk"
-PAC_HOST = "https://prepare-a-case-preprod.apps.live-1.cloud-platform.service.justice.gov.uk"
-GET_CASE_TEMPLATE = f"{CCS_HOST}/case/%s/defendant/%s"
-POST_HEARING_TEMPLATE = f"{CHER_HOST}/hearing/%s"
+CCS_TOKEN = ''
+CCS_CLIENT_ID = os.environ.get('CCS_CLIENT_ID')
+CCS_CLIENT_SECRET = os.environ.get('CCS_CLIENT_SECRET')
+CHER_TOKEN = ''
+CHER_CLIENT_ID = os.environ.get('CHER_CLIENT_ID')
+CHER_CLIENT_SECRET = os.environ.get('CHER_CLIENT_SECRET')
 file_path = os.environ.get('FILE_PATH')
-bad_json_path = "/Users/johnevans/temp/bad_json"
-good_json_path = "/Users/johnevans/temp/good_json"
+
+CCS_HOST = os.environ.get('CCS_HOST', "https://court-case-service-preprod.apps.live-1.cloud-platform.service.justice.gov.uk")
+CHER_HOST = os.environ.get('CHER_HOST', "https://court-hearing-event-receiver-preprod.hmpps.service.justice.gov.uk")
+AUTH_HOST = os.environ.get('AUTH_HOST', 'https://sign-in-preprod.hmpps.service.justice.gov.uk')
+PAC_HOST = os.environ.get('PAC_HOST', "https://prepare-a-case-preprod.apps.live-1.cloud-platform.service.justice.gov.uk")
+
+AUTH_URL = f"{AUTH_HOST}/auth/oauth/token"
+GET_HEARING_TEMPLATE = f"{CCS_HOST}/hearing/%s/defendant/%s"
+POST_HEARING_TEMPLATE = f"{CHER_HOST}/hearing/%s"
 csv.field_size_limit(sys.maxsize)
 
-resume_file_path = f"{file_path}-resume.txt"
-failures_path = f"{file_path}-failures.csv"
+resume_file_path = f"{file_path}.resume.txt"
+failures_path = f"{file_path}.failures.csv"
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -31,19 +38,33 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-
 resume_at = 0
+progress_report_interval = 100
+
 
 def authenticate(client_id, client_secret):
-    pass
+    auth_response = requests.post(
+        AUTH_URL,
+        auth=HTTPBasicAuth(client_id, client_secret),
+        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+        data={
+            'grant_type': 'client_credentials'
+        }
+    )
+    if auth_response.status_code != 200:
+        logging.error(f"Non 200 status getting token: {auth_response.status_code}")
+        logging.error(auth_response.text)
+        raise Exception(f"Non 200 status getting token: {auth_response.status_code}")
+    return auth_response.json()['access_token']
 
 
-def checkpoint(checkpoint):
+def checkpoint(checkpoint_param):
     with open(resume_file_path, 'w') as resume_file:
         global resume_at
-        resume_at = checkpoint
+        resume_at = checkpoint_param
         resume_file.write(str(resume_at))
         logging.debug(f"Next time will resume from {resume_at}")
+
 
 start_time = time.time()
 try:
@@ -60,13 +81,13 @@ try:
         new_case_defendant_count = 0
         known_validation_errors = 0
         post_count = 0
+        sjp_referral_count = 0
 
         skip_to = 0
-        stop_at = 1000
+        stop_at = 1000000
         try:
             with open(resume_file_path, 'r') as f:
                 skip_to = int(f.readline())
-                # stop_at = skip_to
                 logging.info(f"Resuming from {skip_to}")
         except FileNotFoundError as e:
             logging.info(f"No resume.txt, starting from 0")
@@ -76,77 +97,99 @@ try:
             count += 1
             if count < skip_to:
                 logging.debug(f"Skipping {count}")
-                resume_at = count
                 continue
             else:
                 if count > stop_at:
                     logging.debug(f"Stopping now")
                     break
                 logging.debug(f"Processing entry {count}")
+            if count % progress_report_interval == 0:
+                logging.info(f"At record {count} after {time.time() - start_time} seconds")
 
             row_dict = json.loads(row['payload'])
             try:
-                hearing_id = row_dict['id']
-                case_id = row_dict['prosecutionCases'][0]['id']
-                court_code = row_dict.get('courtCentre', {}).get('code', "")[:-2]
+                retry = True
+                while retry:
+                    retry = False
+                    defendant_statuses = []
+                    locations = []
 
-                defendant_statuses = []
-                locations = []
-                for defendant in row_dict['prosecutionCases'][0]['defendants']:
-                    get_url = GET_CASE_TEMPLATE % (case_id, defendant['id'])
-                    r = requests.get(get_url, headers={'Authorization': f"Bearer {COURT_CASE_TOKEN}"})
-                    # logging.debug(f"{r.status_code} from {get_url}")
-                    defendant_statuses += [r.status_code]
-                    if r.status_code == 404:
-                        new_case_defendant_count += 1
+                    hearing_id = row_dict['id']
+                    case_id = row_dict['prosecutionCases'][0]['id']
+                    case_status = row_dict.get('prosecutionCases', [])[0].get('caseStatus', "UNKNOWN")
+                    if case_status == 'SJP_REFERRAL':
+                        logging.debug(f'Hearing {hearing_id} is an SJP_REFERRAL, skipping')
+                        sjp_referral_count += 1
+                        continue
+                    court_code = row_dict.get('courtCentre', {}).get('code', "")[:-2]
+                    for defendant in row_dict['prosecutionCases'][0]['defendants']:
+                        get_url = GET_HEARING_TEMPLATE % (hearing_id, defendant['id'])
+                        r = requests.get(get_url, headers={'Authorization': f"Bearer {CCS_TOKEN}"})
+                        # logging.debug(f"{r.status_code} from {get_url}")
+                        defendant_statuses += [r.status_code]
+                        if r.status_code == 401:
+                            CCS_TOKEN = authenticate(CCS_CLIENT_ID, CCS_CLIENT_SECRET)
+                            retry = True
+                            continue
+                        if r.status_code == 404:
+                            new_case_defendant_count += 1
 
-                    if court_code:
-                        locations.append(f"{PAC_HOST}/{court_code}/case/{case_id}/defendant/{defendant['id']}/summary")
+                        if court_code:
+                            locations.append(
+                                f"{PAC_HOST}/{court_code}/hearing/{hearing_id}/defendant/{defendant['id']}/summary")
 
 
                 def is404(status):
                     return status == 404
 
+
                 def is200(status):
                     return status == 200
 
-
                 if all(map(is404, defendant_statuses)):
-                    body = "{\"hearing\":%s}" % row['payload']
-                    # logging.debug(body)
-                    post_url = POST_HEARING_TEMPLATE % hearing_id
-                    logging.debug(f"Posting {post_url}")
-                    headers = {
-                        'Authorization': f"Bearer {CHER_TOKEN}",
-                        'Content-Type': "application/json"
-                    }
-                    r = requests.post(post_url, headers=headers, data=body.encode('utf-8'))
-                    logging.debug(f"Received {r.status_code} from {post_url}")
-                    post_count += 1
-                    if r.status_code == 200:
-                        logging.info(f"New case will be visible here: {', '.join(locations)}")
-                    else:
-                        if "parameter caseURN which is a non-nullable type" in r.text:
-                            logging.debug(f"Case {case_id} contained no caseURN, skipping")
-                            known_validation_errors += 1
-                            checkpoint(count + 1)
+                    retry = True
+                    while retry:
+                        retry = False
+                        body = "{\"hearing\":%s}" % row['payload']
+                        # logging.debug(body)
+                        post_url = POST_HEARING_TEMPLATE % hearing_id
+                        logging.debug(f"Posting {post_url}")
+                        headers = {
+                            'Authorization': f"Bearer {CHER_TOKEN}",
+                            'Content-Type': "application/json"
+                        }
+                        r = requests.post(post_url, headers=headers, data=body.encode('utf-8'))
+                        logging.debug(f"Received {r.status_code} from {post_url}")
+                        if r.status_code == 401:
+                            CHER_TOKEN = authenticate(CHER_CLIENT_ID, CHER_CLIENT_SECRET)
+                            retry = True
                             continue
-                        if "parameter code which is a non-nullable type" in r.text:
-                            logging.debug(f"Case {case_id} contained no courtCode, skipping")
-                            known_validation_errors += 1
-                            checkpoint(count + 1)
-                            continue
-                        if "parameter roomId which is a non-nullable type" in r.text:
-                            logging.debug(f"Case {case_id} contained no roomId, skipping")
-                            known_validation_errors += 1
-                            checkpoint(count + 1)
-                            continue
-                        raise Exception(r.status_code, r.text)
+                        post_count += 1
+                        if r.status_code == 200:
+                            logging.info(f"New hearing will be visible here: {', '.join(locations)}")
+                        else:
+
+                            if "parameter caseURN which is a non-nullable type" in r.text:
+                                logging.debug(f"Hearing {hearing_id} contained no caseURN, skipping")
+                                known_validation_errors += 1
+                                checkpoint(count + 1)
+                                continue
+                            if "parameter code which is a non-nullable type" in r.text:
+                                logging.debug(f"Hearing {hearing_id} contained no courtCode, skipping")
+                                known_validation_errors += 1
+                                checkpoint(count + 1)
+                                continue
+                            if "parameter roomId which is a non-nullable type" in r.text:
+                                logging.debug(f"Hearing {hearing_id} contained no roomId, skipping")
+                                known_validation_errors += 1
+                                checkpoint(count + 1)
+                                continue
+                            raise Exception(r.status_code, r.text)
 
                 else:
                     if all(map(is200, defendant_statuses)):
-                        logging.debug(f"Case {case_id} already exists, skipping POST")
-                        logging.info(f"Existing case will be visible here: {', '.join(locations)}")
+                        logging.debug(f"Hearing {hearing_id} already exists, skipping POST")
+                        logging.info(f"Existing hearing will be visible here: {', '.join(locations)}")
                         already_exists_count += 1
                         checkpoint(count + 1)
                         continue
@@ -154,7 +197,7 @@ try:
                     failures_writer.writerow(row)
 
                     checkpoint(count)
-                    raise Exception(f"Aborting due to unexpected statuses from GET case/defendant {defendant_statuses}")
+                    raise Exception(f"Aborting due to unexpected statuses from GET hearing/defendant {defendant_statuses}")
 
             except KeyError as e:
                 if e.args[0] == 'prosecutionCases':
@@ -169,7 +212,7 @@ try:
                 failures_writer.writerow(row)
                 # raise e
             except Exception as e:
-                logging.debug(e)
+                logging.error(e)
                 unexpected_error_count += 1
                 checkpoint(count)
                 failures_writer.writerow(row)
@@ -179,6 +222,7 @@ try:
             success_count += 1
 finally:
     logging.info(f"Next time will resume from {resume_at}")
-    logging.info(f"Successfully processed {success_count} cases. There were {already_exists_count} cases which already existed, {known_validation_errors} known validation errors, {unexpected_error_count} unexpected errors and {missing_prosecution_cases_count} hearings with no prosecutionCases", )
+    logging.info(
+        f"Successfully processed {success_count} hearings. There were {already_exists_count} hearings which already existed, {known_validation_errors} known validation errors, {sjp_referral_count} SJP referrals, {unexpected_error_count} unexpected errors and {missing_prosecution_cases_count} hearings with no prosecutionCases", )
     logging.info(f"Attempted posts: {post_count}", )
     logging.info(f"Execution time was {time.time() - start_time} seconds")
